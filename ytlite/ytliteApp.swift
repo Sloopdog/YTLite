7505b01c342678bc2b9ad248df522e09393056f3
Sloopdog <sloopdog06@yahoo.com>
2026-07-01 10:56:37 -0400

    Initial Commit

---
+++ ytlite/ytliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ytliteApp.swift
+//  ytlite
+//
+//  Created by Michael Sloop on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ytliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ytliteTests/ytliteTests.swift
@@ -0,0 +1,17 @@
+//
+//  ytliteTests.swift
+//  ytliteTests
+//
+//  Created by Michael Sloop on 7/1/26.
+//
+
+import Testing
+@testable import ytlite
+
+struct ytliteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
